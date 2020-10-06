dbeebd7550e962e144c86946b9be3a5794c2ed75
dima <dimich3d@ya.ru>
2020-10-06 22:48:46 +0300

    add model layers

--- training/model.py
+++ training/model.py
@@ -3,6 +3,8 @@ from logging import basicConfig, DEBUG
 from tensorflow.python.keras import Input
 import tensorflow as tf
 import tensorflow_hub as hub
+from tensorflow.python.keras.layers import Dense, concatenate
+
 from preprocessing.complaints_preporcessing import ONE_HOT_FEATURES, transformed_name, BUCKET_FEATURES, TEXT_FEATURES
 
 
@@ -20,6 +22,14 @@ def get_model():
     model_inputs = input_features + input_texts
     MODULE_URL = "https://tfhub.dev/google/universal-sentence-encoder/4"
     embed = hub.KerasLayer(MODULE_URL)
+    embed_narrative = embed(tf.reshape(input_texts[0], [-1]))
+    deep = Dense(256, activation='relu')(embed_narrative)
+    deep = tf.keras.layers.Dense(64, activation='relu')(deep)
+    deep = tf.keras.layers.Dense(16, activation='relu')(deep)
+    wide_ff = concatenate(input_features)
+    wide = tf.keras.layers.Dense(16, activation='relu')(wide_ff)
+    both = concatenate([deep, wide])
+
     return model_inputs
 
 
